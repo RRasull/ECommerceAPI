84403dd7a88787b160cdbf865543f5b5ff03d0cf
RRasull <92165344+RRasull@users.noreply.github.com>
2022-06-11 18:55:05 +0400

    'feat: add Products entity'

---
+++ Core/ECommerceAPI.Domain/Entities/Products.cs
@@ -0,0 +1,16 @@
+﻿using ECommerceAPI.Domain.Entities.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ECommerceAPI.Domain.Entities
+{
+    public class Products : BaseEntity
+    {
+        public string Name { get; set; }
+        public int Stock { get; set; }
+        public double Price { get; set; }
+    }
+}
