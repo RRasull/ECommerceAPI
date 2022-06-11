b15afb89cd3df1e943ba227f104d5d59c3df98d3
RRasull <92165344+RRasull@users.noreply.github.com>
2022-06-11 20:24:31 +0400

    'feat: add PostgreSQL connection string '

---
+++ Infrastructure/ECommerceAPI.Persistence/DAL/ECommerceAppDbContext.cs
@@ -0,0 +1,20 @@
+﻿using ECommerceAPI.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ECommerceAPI.Persistence.DAL
+{
+    public class ECommerceAppDbContext : DbContext
+    {
+        public ECommerceAppDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<Order> Orders { get; set; }
+
+    }
+}
---
+++ Infrastructure/ECommerceAPI.Persistence/ServiceRegistiration.cs
@@ -0,0 +1,23 @@
+﻿using ECommerceAPI.Persistence.DAL;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ECommerceAPI.Persistence
+{
+    public static class ServiceRegistiration
+    {
+        public static void AddPersistenceServices(this IServiceCollection services)
+        {
+            services.AddDbContext<ECommerceAppDbContext>(options =>
+            {
+                options.UseNpgsql("User ID=postgres;Password=123456;Host=localhost;Port=5432;Database=ECommerceAPIDb");
+            });
+        }
+    }
+}
