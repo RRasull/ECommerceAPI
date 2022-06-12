9e7d1a834ac959de12e2b6ce1e3824881bf185c8
RRasull <92165344+RRasull@users.noreply.github.com>
2022-06-13 02:32:08 +0400

    'feat: add Repository interfaces'

---
+++ Core/ECommerceAPI.Application/Repositories/ICommandRepository.cs
@@ -0,0 +1,13 @@
+﻿using ECommerceAPI.Domain.Entities.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ECommerceAPI.Application.Repositories
+{
+    public interface ICommandRepository<TEntity> : IRepository<TEntity> where TEntity : BaseEntity
+    {
+    }
+}
---
+++ Core/ECommerceAPI.Application/Repositories/IQueryRepository.cs
@@ -0,0 +1,14 @@
+﻿using ECommerceAPI.Domain.Entities.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ECommerceAPI.Application.Repositories
+{
+    public interface IQueryRepository<TEntity> : IRepository<TEntity> where TEntity : BaseEntity
+    {
+
+    }
+}
