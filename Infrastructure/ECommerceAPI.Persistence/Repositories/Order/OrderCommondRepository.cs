15b8bb832ad68cc5cb0a95b343b6d51151ca8463
RRasull <92165344+RRasull@users.noreply.github.com>
2022-06-14 19:07:21 +0400

    'feat: add Order repository'

---
+++ Infrastructure/ECommerceAPI.Persistence/Repositories/Order/OrderCommondRepository.cs
@@ -0,0 +1,18 @@
+﻿using ECommerceAPI.Application.Repositories;
+using ECommerceAPI.Domain.Entities;
+using ECommerceAPI.Persistence.DAL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ECommerceAPI.Persistence.Repositories
+{
+    public class OrderCommondRepository : CommandRepository<Order>, IOrderCommondRepository
+    {
+        public OrderCommondRepository(ECommerceAppDbContext context) : base(context)
+        {
+        }
+    }
+}
---
+++ Infrastructure/ECommerceAPI.Persistence/Repositories/Order/OrderQueryRepository.cs
@@ -0,0 +1,18 @@
+﻿using ECommerceAPI.Application.Repositories;
+using ECommerceAPI.Domain.Entities;
+using ECommerceAPI.Persistence.DAL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ECommerceAPI.Persistence.Repositories
+{
+    public class OrderQueryRepository : QueryRepository<Order>, IOrderQueryRepository
+    {
+        public OrderQueryRepository(ECommerceAppDbContext context) : base(context)
+        {
+        }
+    }
+}
