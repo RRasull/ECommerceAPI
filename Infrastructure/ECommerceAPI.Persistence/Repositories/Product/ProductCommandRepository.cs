464e5410dfaf1ab8f55dad6abe2554ceb1c987af
RRasull <92165344+RRasull@users.noreply.github.com>
2022-06-14 19:11:35 +0400

    'feat: add Product repository'

---
+++ Infrastructure/ECommerceAPI.Persistence/Repositories/Product/ProductCommandRepository.cs
@@ -0,0 +1,18 @@
+﻿using ECommerceAPI.Application.Repositories;
+using ECommerceAPI.Domain.Entities;
+using ECommerceAPI.Persistence.DAL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ECommerceAPI.Persistence.Repositories
+{
+    public class ProductCommandRepository : CommandRepository<Product>, IProductCommandRepository
+    {
+        public ProductCommandRepository(ECommerceAppDbContext context) : base(context)
+        {
+        }
+    }
+}
