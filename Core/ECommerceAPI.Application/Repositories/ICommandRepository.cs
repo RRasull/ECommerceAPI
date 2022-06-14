bb791d4aa1f889651933191d6362606ef8e2f180
RRasull <92165344+RRasull@users.noreply.github.com>
2022-06-14 17:58:54 +0400

    'feat: add ICommandRepository interface'

--- Core/ECommerceAPI.Application/Repositories/ICommandRepository.cs
+++ Core/ECommerceAPI.Application/Repositories/ICommandRepository.cs
@@ -9,5 +9,11 @@ namespace ECommerceAPI.Application.Repositories
 {
     public interface ICommandRepository<TEntity> : IRepository<TEntity> where TEntity : BaseEntity
     {
+        Task<bool> AddAsync(TEntity model);
+        Task<bool> AddRangeAsync(List<TEntity> model);
+        Task<bool> DeleteAsync(TEntity model);
+        Task<bool> DeleteByIdAsync(string id);
+        Task<bool> UpdateAsync(TEntity model);
+
     }
 }
