2c7f03f8236a1a9ea0e4d163ef11d886a11ee679
RRasull <92165344+RRasull@users.noreply.github.com>
2022-06-14 18:58:41 +0400

    'feat: add Customer repository'

---
+++ Infrastructure/ECommerceAPI.Persistence/Repositories/Customer/CustomerCommandRepository.cs
@@ -0,0 +1,19 @@
+﻿using ECommerceAPI.Application.Repositories;
+using ECommerceAPI.Domain.Entities;
+using ECommerceAPI.Persistence.DAL;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ECommerceAPI.Persistence.Repositories
+{
+    public class CustomerCommandRepository : CommandRepository<Customer>, ICustomerCommandRepository
+    {
+        public CustomerCommandRepository(ECommerceAppDbContext context) : base(context)
+        {
+        }
+    }
+}
---
+++ Infrastructure/ECommerceAPI.Persistence/Repositories/Customer/CustomerQueryRepository.cs
@@ -0,0 +1,20 @@
+﻿using ECommerceAPI.Application.Repositories;
+using ECommerceAPI.Domain.Entities;
+using ECommerceAPI.Persistence.DAL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ECommerceAPI.Persistence.Repositories
+{
+    public class CustomerQueryRepository : QueryRepository<Customer>, ICustomerQueryRepository
+    {
+        public CustomerQueryRepository(ECommerceAppDbContext context) : base(context)
+        {
+        }
+
+
+    }
+}
